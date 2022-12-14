1899f80be7dcdc707e272e110db46c9b453a7b30
朝日薫 <askn37@users.noreply.github.com>
2022-12-14 10:07:44 +0900

    fix tabsize

--- examples/M5_TOF/M5_TOF.ino
+++ examples/M5_TOF/M5_TOF.ino
@@ -14,41 +14,41 @@
 TOF_VL53L0X_Class TOF = {Wire};
 
 void setup (void) {
-	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.begin(CONSOLE_BAUD).println(F("\r<startup>"));
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(CONSOLE_BAUD).println(F("\r<startup>"));
 
-	Wire.initiate(TWI_SM, false);
+  Wire.initiate(TWI_SM, false);
 
-	if ( !TOF.initialize() ) {
-		Serial.println(F("nodevice"));
-		for (;;);
-	}
+  if ( !TOF.initialize() ) {
+    Serial.println(F("nodevice"));
+    for (;;);
+  }
 
-	loop_until_bit_is_clear(RTC_PITSTATUS, RTC_CTRLBUSY_bp);
-	RTC_PITINTCTRL = RTC_PI_bm;
-	RTC_PITCTRLA = RTC_PITEN_bm | RTC_PERIOD_CYC16384_gc;
+  loop_until_bit_is_clear(RTC_PITSTATUS, RTC_CTRLBUSY_bp);
+  RTC_PITINTCTRL = RTC_PI_bm;
+  RTC_PITCTRLA = RTC_PITEN_bm | RTC_PERIOD_CYC16384_gc;
 
-	set_sleep_mode(SLEEP_MODE_STANDBY);
-	sleep_enable();
+  set_sleep_mode(SLEEP_MODE_STANDBY);
+  sleep_enable();
 }
 
 ISR(RTC_PIT_vect) {
-	RTC_PITINTFLAGS = RTC_PI_bm;
+  RTC_PITINTFLAGS = RTC_PI_bm;
 }
 
 void loop (void) {
-	digitalWrite(LED_BUILTIN, TOGGLE);
+  digitalWrite(LED_BUILTIN, TOGGLE);
 
-	if ( TOF.update() && TOF.getDistance() > 0 ) {
-		Serial.print(F(",Dist=")).print(TOF.getDistance());
-		Serial.print(':').println(TOF.getDistance());
-	}
-	else {
-		Serial.println(F(",Dist=nan:nan"));
-	}
+  if ( TOF.update() && TOF.getDistance() > 0 ) {
+    Serial.print(F(",Dist=")).print(TOF.getDistance());
+    Serial.print(':').println(TOF.getDistance());
+  }
+  else {
+    Serial.println(F(",Dist=nan:nan"));
+  }
 
-	Serial.flush();
-	sleep_cpu();
+  Serial.flush();
+  sleep_cpu();
 }
 
 // end of code
